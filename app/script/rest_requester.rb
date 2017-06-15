555181a7a3321367bd7eba0357176571717239d8
JrodManU <jrodmanu@yahoo.com>
2017-06-14 22:14:29 -0400

    finished exercise

---
+++ app/script/rest_requester.rb
@@ -0,0 +1,7 @@
+require 'rest-client'
+url = "http://localhost:3000/users"
+puts RestClient.get("http://localhost:3000/users")
+puts RestClient.get("http://localhost:3000/users/1")
+puts RestClient.get("http://localhost:3000/users/new")
+puts RestClient.get("http://localhost:3000/users/1/edit")
+puts RestClient.post("http://localhost:3000/users", "")
